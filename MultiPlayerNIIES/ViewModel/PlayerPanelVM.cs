2833a6768def17f2d2429c2588ee5953c10ee972
lvovandrey <lvovandrey@mail.ru>
2019-08-13 22:27:55 +0300

    Сделал заготовку под VM PlayerPanel Начал пилить интерфейс IUniversalPlayer

--- MultiPlayerNIIES/ViewModel/PlayerPanelVM.cs
+++ MultiPlayerNIIES/ViewModel/PlayerPanelVM.cs
@@ -10,18 +10,104 @@ namespace MultiPlayerNIIES.ViewModel
 
     public class PlayerPanelVM : INPCBase
     {
+
+        #region Поля 
+
+        #endregion
+
+        #region Конструкторы
+        #endregion
+
+        #region Методы
+        #endregion
+
+        #region Свойства
         private double position { get; set; }
         [Magic]
         public double Position { get { return position; } set { position = value; } }
 
-        public int volume { get; set; }
-        public int Volume { get; set; }
+        public double volume { get; set; }
+        [Magic]
+        public double Volume { get { return volume; } set { volume = value; } }
 
-        public int curTime { get; set; }
-        public int CurTime { get; set; }
+        public TimeSpan curTime { get; set; }
+        [Magic]
+        public TimeSpan CurTime { get { return curTime; } set { curTime = value; } }
+        #endregion
 
 
+        #region Комманды
+        /* Просто добавь ctrl+v =) 
+        private RelayCommand Command;  
+        public RelayCommand Command { get { return Command ?? (Command = new RelayCommand(obj =>{ 
+        }));}}             */
 
+        private RelayCommand playPauseCommand;
+        public RelayCommand PlayPauseCommand
+        {
+            get
+            {
+                return playPauseCommand ?? (playPauseCommand = new RelayCommand(obj =>
+                {
+
+                }));
+            }
+        }
+
+
+        private RelayCommand muteCommand;
+        public RelayCommand MuteCommand
+        {
+            get
+            {
+                return muteCommand ?? (muteCommand = new RelayCommand(obj =>
+                {
+                }));
+            }
+        }
+
+        private RelayCommand decSpeedCommand;
+        public RelayCommand DecSpeedCommand
+        {
+            get
+            {
+                return decSpeedCommand ?? (decSpeedCommand = new RelayCommand(obj => {
+                }));
+            }
+        }
+
+        private RelayCommand incSpeedCommand;
+        public RelayCommand IncSpeedCommand
+        {
+            get
+            {
+                return incSpeedCommand ?? (incSpeedCommand = new RelayCommand(obj => {
+                }));
+            }
+        }
+
+        private RelayCommand stepBackwardCommand;
+        public RelayCommand StepBackwardCommand
+        {
+            get
+            {
+                return stepBackwardCommand ?? (stepBackwardCommand = new RelayCommand(obj => {
+                }));
+            }
+        }
+
+        private RelayCommand stepForwardCommand;
+        public RelayCommand StepForwardCommand
+        {
+            get
+            {
+                return stepForwardCommand ?? (stepForwardCommand = new RelayCommand(obj => {
+                }));
+            }
+        }
+
+
+        #endregion
 
 
     }
