9029fc2bcdee3842194033f476d4423c447b8dbc
lvovandrey <lvovandrey@mail.ru>
2019-08-14 04:12:20 +0300

    тестовое ПО для обмена сообщениями с excel добавил.

---
+++ WpfPostMessage/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPostMessage
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HwndSource source;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            //  do stuff
+            Txt.Text += msg.ToString() + "/" + lParam.ToInt32().ToString() + "/" + wParam.ToInt32().ToString() + " - ";
+            Thread.Sleep(50);
+            return IntPtr.Zero;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+    }
+}
