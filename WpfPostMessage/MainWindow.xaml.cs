16c5029731e9ab43125f87e6b00861e24b753a81
lvovandrey <lvovandrey@mail.ru>
2019-08-14 17:26:24 +0300

    Добавил прямую связь - отсылку сообщений в excel. Потестил = все работает

--- WpfPostMessage/MainWindow.xaml.cs
+++ WpfPostMessage/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WpfPostMessage
 {
@@ -35,15 +36,32 @@ namespace WpfPostMessage
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             //  do stuff
-            Txt.Text += msg.ToString() + "/" + lParam.ToInt32().ToString() + "/" + wParam.ToInt32().ToString() + " - ";
-            Thread.Sleep(50);
+            try
+            {
+                Txt.Text += msg.ToString() + "/" + lParam.ToInt32().ToString() + "/" + wParam.ToInt32().ToString() + " - ";
+            }
+            catch
+            { }
+            //        Thread.Sleep(50);
             return IntPtr.Zero;
         }
-
+        Excel.Application ex;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
             source.AddHook(new HwndSourceHook(WndProc));
+
+            ex = new Excel.Application();
+            ex.Visible = true;
+            Excel.Workbooks books = ex.Workbooks;
+            Excel._Workbook book = null;
+            book = books.Open(Environment.CurrentDirectory + @"\test.xlsm");
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ex.Run((object)"CurModeReceive", (object)"FuckMode");
         }
     }
 }
