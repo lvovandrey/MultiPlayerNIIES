d0c1365e9fe5ca96305e87b294931f0ef3f78ff9
lvovandrey <lvovandrey@mail.ru>
2019-11-24 13:36:01 +0300

    Убрал лишние вьюшки. Добавил инфо-окно - Фича 020

--- MultiPlayerNIIES/View/InfoWindowView.xaml.cs
+++ MultiPlayerNIIES/View/InfoWindowView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MultiPlayerNIIES.View
 {
     /// <summary>
-    /// Логика взаимодействия для VideoPlayerPropertiesView.xaml
+    /// Логика взаимодействия для InfoWindowView.xaml
     /// </summary>
-    public partial class VideoPlayerPropertiesView : UserControl
+    public partial class InfoWindowView : Window
     {
-        public VideoPlayerPropertiesView()
+        public InfoWindowView()
         {
             InitializeComponent();
         }
