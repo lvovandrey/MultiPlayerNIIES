a05b95b42b14a8da7844ca3465f706fd451f8aea
lvovandrey <lvovandrey@mail.ru>
2019-12-30 00:53:39 +0300

    Доделал слайдер с индикацией смещения. Все еще пилим мега фичу 029

---
+++ MultiPlayerNIIES/View/ValueConverters/SyncShiftSliderValueToMargin_MultiConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MultiPlayerNIIES.View.ValueConverters
+{
+    class SyncShiftSliderValueToMargin_MultiConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var val = (double)values[0];
+            var maxval = (double)values[1];
+            var minval = (double)values[2];
+            var w = (double)values[3];
+
+            double center = w * (-minval / (maxval - minval));
+
+            Thickness marg = new Thickness();
+            double k = val / (maxval - minval);
+            double pos = k * w;
+            if (val > 0)
+                marg = new Thickness(center, 0, w - center - pos + 5, 0);
+            else
+                marg = new Thickness(center + pos + 5, 0, w - center, 0);
+
+            return marg;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
