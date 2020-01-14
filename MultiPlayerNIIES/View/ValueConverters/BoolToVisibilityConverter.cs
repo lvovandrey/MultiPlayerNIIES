cb2cedf5b3e56819ac61f820b32e54621304a548
Lvov Andrey <lvovandrey@mail.ru>
2020-01-14 17:29:35 +0300

    Фича 037 038 039 в процессе (Меню-Вид-Скрыть панели управления/ Минималистичный вид панели управления)

--- MultiPlayerNIIES/View/ValueConverters/BoolToVisibilityConverter.cs
+++ MultiPlayerNIIES/View/ValueConverters/BoolToVisibilityConverter.cs
@@ -43,4 +43,21 @@ namespace MultiPlayerNIIES.View.ValueConverters
             return DependencyProperty.UnsetValue;
         }
     }
+
+    class BoolToVisibilityToCollapseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
 }
