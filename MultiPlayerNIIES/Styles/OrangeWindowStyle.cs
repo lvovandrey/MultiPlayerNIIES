73dd4cfb2a3158afe6beaa062c1759bf8a84913b
Lvov Andrey <lvovandrey@mail.ru>
2020-01-17 17:30:10 +0300

    Общий стиль окон - сделал. Фича 031. Создать свои диалоговые окна со своим стилем.

---
+++ MultiPlayerNIIES/Styles/OrangeWindowStyle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace MultiPlayerNIIES.Styles
+{
+
+    internal static class LocalExtensions
+    {
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+
+        public static IntPtr GetWindowHandle(this Window window)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+
+    public partial class OrangeWindowStyle
+    {
+        void DraggerMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                sender.ForWindowFromTemplate(w => w.DragMove());
+            }
+        }
+
+        void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => SystemCommands.CloseWindow(w));
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            string s = (sender as Hyperlink).NavigateUri.OriginalString;
+            Process.Start(s);
+        }
+    }
+}
